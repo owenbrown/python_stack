1e1feadb80a7d1cf77e17f7a510cdc14b379e01e
Owen Brown <owen@starbutter.com>
2019-05-07 12:31:29 -0700

    Assignment: Math Dojo TDD

--- math_dojo.py
+++ math_dojo.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class MathDojo:
     def __init__(self):
         self.result = 0
@@ -14,9 +17,18 @@ class MathDojo:
             self.result -= n
         return self
 
-# create an instance:
-md = MathDojo()
-# to test:
-x = md.add(2).add(2, 5, 1).subtract(3, 2).result
-print(x)  # should print 5
-# run each of the methods a few more times and check the result!
+
+class TestMathDojo(unittest.TestCase):
+
+    def setUp(self):
+        self.md = MathDojo()
+
+    def test_add_subtract(self):
+        self.assertEqual(self.md.add(2).add(2, 5, 1).subtract(3, 2).result, 5)
+
+    def test_add(self):
+        self.assertEqual(self.md.add(2).result, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
