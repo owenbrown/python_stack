4f0b95b1934b70ea1a77ababb1ec32b2763013a9
Owen Brown <owen@starbutter.com>
2019-05-07 08:27:41 -0700

    Assignment: User

--- user.py
+++ user.py
@@ -2,8 +2,16 @@ class User(object):
     def __init__(self, opening_balance):
         self.balance = opening_balance
 
-    def make_withdrawal(self, amount) -> bool:
+    def make_deposit(self, amount) -> bool:
+        if amount <= 0:
+            return False
+        else:
+            self.balance += amount
+            return True
 
+    def make_withdrawal(self, amount) -> bool:
+        if amount <= 0:
+            return False
         if amount > self.balance:
             return False
         else:
@@ -21,3 +29,25 @@ class User(object):
             self.balance -= amount
             return True
 
+larry = User(10)
+curly = User(5)
+moe = User(3)
+
+larry.make_deposit(1)
+larry.make_deposit(1)
+larry.make_deposit(1)
+larry.make_withdrawal(5)
+larry.display_user_balance()
+
+curly.make_deposit(5)
+curly.make_deposit(5)
+curly.make_withdrawal(3)
+curly.make_withdrawal(3)
+curly.display_user_balance()
+
+moe.make_deposit(34)
+moe.display_user_balance()
+
+larry.transfer_money(moe, 2)
+larry.display_user_balance()
+moe.display_user_balance()
