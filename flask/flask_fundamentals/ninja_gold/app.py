28442b116caacaf2d8e9badc2aa55653dbbb2bc6
Owen Brown <owen@starbutter.com>
2019-05-09 10:09:25 -0700

    WIP - Assignment: Ninja Gold

--- flask/flask_fundamentals/ninja_gold/app.py
+++ flask/flask_fundamentals/ninja_gold/app.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from flask import Flask, render_template, redirect, request, session
@@ -24,7 +25,8 @@ def process_money():
     delta = places[place_name].find_gold()
     session['gold'] = session.get('gold', 0) + delta
 
-    msg = f"Your earned {delta} gold at the {place_name}. Total gold is {session['gold']}"
+    msg = f"Your earned {delta} gold at the {place_name}. " \
+        f"Total gold is {session['gold']} ({datetime.datetime.utcnow().strftime('%I:%M')} UTC)"
     if 'messages' in session:
         session['messages'].append(msg)
     else:
