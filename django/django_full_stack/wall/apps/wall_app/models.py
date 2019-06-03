d876d053a9c8397b12c4528a72c346cd8d3a718a
Owen Brown <owen@starbutter.com>
2019-06-03 10:04:37 -0700

    Assignment: Wall Initialize and add models.py

---
+++ django/django_full_stack/wall/apps/wall_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    message_id = models.ForeignKey(Message, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
